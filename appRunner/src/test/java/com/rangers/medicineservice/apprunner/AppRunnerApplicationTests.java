afcd70eb7b7b079c3ad34473000e411eac27f5f0
Oleksii <achili3f17@gmail.com>
2024-04-28 17:28:28 +0200

    create modules

---
+++ appRunner/src/test/java/com/rangers/medicineservice/apprunner/AppRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rangers.medicineservice.apprunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRunnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ telegramBot/src/test/java/com/rangers/medicineservice/telegrambot/TelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rangers.medicineservice.telegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
